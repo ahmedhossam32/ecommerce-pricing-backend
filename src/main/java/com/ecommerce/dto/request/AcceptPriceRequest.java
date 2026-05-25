8dada58591b7b92c74a15f08f363d80a79b279c1
ahmedhossam32 <ahmedhemly2020@yahoo.com>
2026-05-25 14:03:19 +0300

    fix: add @Positive validation to AcceptPriceRequest.chosenPrice

--- src/main/java/com/ecommerce/dto/request/AcceptPriceRequest.java
+++ src/main/java/com/ecommerce/dto/request/AcceptPriceRequest.java
@@ -1,8 +1,10 @@
 package com.ecommerce.dto.request;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AcceptPriceRequest {
+    @Positive
     private Double chosenPrice;
 }
